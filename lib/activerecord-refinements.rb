f6dec2011974ee72b70ebd5af45a5dd7346e4f9b
Akira Matsuda <ronnie@dio.jp>
2012-09-28 19:34:28 +0900

    extract the patch into another file

--- lib/activerecord-refinements.rb
+++ lib/activerecord-refinements.rb
@@ -1,60 +1,8 @@
 require 'activerecord-refinements/version'
 require 'active_record'
+require 'active_record/refinements'
 
 module ActiveRecord
-  module Refinements
-    module WhereBlockSyntax
-      refine Symbol do
-        %i[== != =~ > >= < <=].each do |op|
-          define_method(op) {|val| [self, op, val] }
-        end
-      end
-    end
-
-    class WhereBlockEvaluator
-      using ActiveRecord::Refinements::WhereBlockSyntax
-
-      def initialize(table)
-        @table = table
-      end
-
-      def evaluate(&block)
-        col, op, val = instance_eval &block
-        case op
-        when :==
-          @table[col].eq val
-        when :!=
-          @table[col].not_eq val
-        when :=~
-          @table[col].matches val
-        when :>
-          @table[col].gt val
-        when :>=
-          @table[col].gteq val
-        when :<
-          @table[col].lt val
-        when :<=
-          @table[col].lteq val
-        else
-          raise "unexpected op: #{op}"
-        end
-      end
-    end
-
-    module QueryMethods
-      def where(opts = nil, *rest, &block)
-        if block
-          evaluator = ActiveRecord::Refinements::WhereBlockEvaluator.new(table)
-          clone.tap do |relation|
-            relation.where_values += build_where(evaluator.evaluate(&block))
-          end
-        else
-          super
-        end
-      end
-    end
-  end
-
   module QueryMethods
     prepend ActiveRecord::Refinements::QueryMethods
   end
